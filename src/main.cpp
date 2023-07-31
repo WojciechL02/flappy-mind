50dd80bc015926561dd8f3d03c5dc0228ab34f70
Wojciech Lapacz <wojciech.lapacz02@gmail.com>
2023-07-31 15:38:47 +0200

    window resized

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-    sf::RenderWindow win(sf::VideoMode(400, 400), "Flappy Mind");
+    sf::RenderWindow win(sf::VideoMode(600, 800), "Flappy Mind");
     Application app(win);
     app.run();
     return 0;
