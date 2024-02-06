5a4306df1227d60b96e470da272e364c36b7779c
Wojciech Lapacz <wojciech.lapacz02@gmail.com>
2024-02-06 13:50:25 +0100

    game state class for RL agents implemented

--- src/GameState.cpp
+++ src/GameState.cpp
@@ -1,3 +1,68 @@
 #include "GameState.h"
+#include <iostream>
 
-GameState::GameState(sf::Event &event) : event(event) {}
+GameState::GameState(sf::Event &event, Bird &bird, std::vector<Pipe> &pipes, double reward, bool isTerminal) : event(event), bucketingParam(30) {
+    const Pipe &nextPipe = findNextPipe(bird, pipes);
+    distanceX = calculateDistanceX(bird, nextPipe);
+    distanceY = calculateDistanceY(bird, nextPipe);
+    distanceBase = calculateDistanceBase(bird);
+    isAbovePipe = isBirdAbove(bird, nextPipe);
+    isUpflap = isBirdUpflap(bird);
+    this->reward = reward;
+    this->isTerminal = isTerminal;
+}
+
+std::vector<int> GameState::getState() {
+//    return std::vector<int> {distanceX, distanceY, distanceBase, isAbovePipe, isUpflap};
+    return std::vector<int> {distanceX, distanceY, isAbovePipe};
+}
+
+double GameState::getReward() {
+    return reward;
+}
+
+bool GameState::getIsTerminal() {
+    return isTerminal;
+}
+
+Pipe GameState::findNextPipe(Bird &bird, std::vector<Pipe> &pipes) {
+    for (auto &pipe : pipes) {
+        if (bird.birdSprite.getGlobalBounds().left < pipe.getRightBound()) {
+            return pipe;
+        }
+    }
+}
+
+int GameState::calculateDistanceX(Bird &bird, const Pipe &pipe) {
+//    if (pipe.spriteDown.getGlobalBounds().left <= bird.birdSprite.getGlobalBounds().left) {
+//        return 0;
+//    }
+    int res = static_cast<int> ((pipe.spriteDown.getGlobalBounds().left - bird.getRightBound()) / bucketingParam);
+    if (res < 0) {
+        return 0;
+    }
+    return res;
+}
+
+int GameState::calculateDistanceY(Bird &bird, const Pipe &pipe) {
+    int value = static_cast<int> (pipe.spriteDown.getGlobalBounds().top - bird.getDownBound());
+    return abs(value) / bucketingParam;
+}
+
+int GameState::calculateDistanceBase(Bird &bird) {
+    return static_cast<int> ((400.0 - bird.getDownBound()) / bucketingParam);
+}
+
+int GameState::isBirdAbove(Bird &bird, const Pipe &pipe) {
+    if (pipe.spriteDown.getGlobalBounds().top > bird.getDownBound()) {
+        return 1;
+    }
+    return 0;
+}
+
+int GameState::isBirdUpflap(Bird &bird) {
+    if (bird.getVelocity() < 0.0) {
+        return 1;
+    }
+    return 0;
+}
--- src/GameState.h
+++ src/GameState.h
@@ -1,8 +1,27 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <vector>
+#include "Bird.h"
+#include "Pipe.h"
 
 class GameState {
 public:
-    GameState(sf::Event &event);
+    GameState(sf::Event &event, Bird &bird, std::vector<Pipe> &pipes, double reward, bool isTerminal);
+    std::vector<int> getState();
+    double getReward();
+    bool getIsTerminal();
+
     sf::Event &event;
+
+private:
+    int bucketingParam, distanceX, distanceY, distanceBase, isAbovePipe, isUpflap;
+    double reward;
+    bool isTerminal;
+
+    Pipe findNextPipe(Bird &bird, std::vector<Pipe> &pipes);
+    int calculateDistanceX(Bird &bird, const Pipe &pipe);
+    int calculateDistanceY(Bird &bird, const Pipe &pipe);
+    int calculateDistanceBase(Bird &bird);
+    int isBirdAbove(Bird &bird, const Pipe &pipe);
+    int isBirdUpflap(Bird &bird);
 };
